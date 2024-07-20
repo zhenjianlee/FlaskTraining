d6882c603d77fbb53b729b5f1d38639c16197576
zhenjianlee <zhenjianlee@outlook.sg>
2024-07-20 20:22:01 +0800

    initialize repo

---
+++ db.py
@@ -0,0 +1,17 @@
+import uuid
+
+one = uuid.uuid4().hex
+two=  uuid.uuid4().hex
+three= uuid.uuid4().hex
+
+stores = {
+    one:'Fairprice',
+    two:'Challenger',
+    three: 'Abloy',
+}
+
+items = {
+    one: ['Food', 'Groceries','Daily use'],
+    two: ['Laptop','Mobiles','Electronics'],
+    three: ['Locks', 'Smart Locks'],
+}
